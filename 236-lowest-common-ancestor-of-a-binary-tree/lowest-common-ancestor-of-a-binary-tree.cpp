bf8537ff5a4bca6341e6eccc3d21282e5ed64110
sourav upreti <146223820+souravupreti@users.noreply.github.com>
2026-03-01 14:52:07 +0530

    Time: 16 ms (27.00%) | Memory: 18.9 MB (5.33%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool findPath(TreeNode* root, TreeNode* target, vector<TreeNode*>& path) {
+        if (root == NULL) return false;
+
+        path.push_back(root);
+
+        if (root == target) return true;
+
+        if (findPath(root->left, target, path) || 
+            findPath(root->right, target, path))
+            return true;
+
+        path.pop_back();   
+        return false;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode *>a;
+        vector<TreeNode *>b;
+        findPath(root,p,a);
+        findPath(root,q,b);
+        TreeNode* ans;
+        int i=0,j=0;
+        while(i<a.size()&&j<b.size()){
+            if(a[i] == b[i]){
+                ans=a[i];
+            }
+            i++;j++;
+        }
+        return ans;
+
+
+    }
+};
