6a4fac9bfe4c01cec7dbbe28cba317950062cb5d
sourav upreti <146223820+souravupreti@users.noreply.github.com>
2026-03-20 18:21:26 +0530

    Time: 32 ms (24.35%) | Memory: 19.3 MB (34.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,51 +1,36 @@
 class Solution {
 public:
-
-    void dfs(int row, int col,
-             vector<vector<char>>& grid,
-             vector<vector<int>>& vis) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vis[row][col] = 1;
-
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, -1, 0, 1};
-
-        for(int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-
-            if(nrow >= 0 && nrow < n &&
-               ncol >= 0 && ncol < m &&
-               !vis[nrow][ncol] &&
-               grid[nrow][ncol] == '1') {
-
-                dfs(nrow, ncol, grid, vis);
-            }
-        }
-    }
-
+    
     int numIslands(vector<vector<char>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        int count = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-
-                if(!vis[i][j] && grid[i][j] == '1') {
+        vector<vector<int>>v(grid.size(),vector<int>(grid[0].size(),0));
+        queue<pair<int,int>>q;
+        int count=0;
+         vector<int>r={1,-1,0,0};
+        vector<int>c={0,0,1,-1};
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(v[i][j]==0&&grid[i][j]=='1'){
                     count++;
-                    dfs(i, j, grid, vis);
+                    q.push({i,j});
+                    v[i][j]=1;
+                    while(!q.empty()){
+                        int a=q.front().first;
+                        int b=q.front().second;
+                        q.pop();
+
+                        for(int i=0;i<4;i++){
+                            int newi=a+r[i];
+                            int newj=b+c[i];
+                            if(newi>=0&&newj>=0&&newi<grid.size()&&newj<grid[0].size()&&v[newi][newj]!=1&&grid[newi][newj]=='1'){
+                    q.push({newi,newj});
+                    v[newi][newj]=1;
+                }
+                        }
+                    }
                 }
             }
         }
-
         return count;
+
     }
 };
